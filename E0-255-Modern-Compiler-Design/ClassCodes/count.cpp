61c785292d53d5e64a3ab9f7da4a97a7a4d3266b
Kuldeep9071 <kuldeepjatav@iisc.ac.in>
2026-01-21 23:17:21 +0530

    learned visualizing CFG and LLVM IR

---
+++ E0-255-Modern-Compiler-Design/ClassCodes/count.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+	int n = 100;
+	int count = 0;
+	for(int i=0;i<n;i++){
+		count++;
+	}
+
+	std::cout << count << '\n';
+
+	return 0;
+}
---
+++ E0-255-Modern-Compiler-Design/ClassCodes/main.s
@@ -0,0 +1,20 @@
+	.file	"main.cpp"
+	.text
+	.globl	main                            # -- Begin function main
+	.p2align	4
+	.type	main,@function
+main:                                   # @main
+.Lmain$local:
+	.type	.Lmain$local,@function
+	.cfi_startproc
+# %bb.0:
+	movl	$0, -4(%rsp)
+	xorl	%eax, %eax
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.size	.Lmain$local, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
